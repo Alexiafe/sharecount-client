3321e7642a60a1c60b79cb42aa5c7660ab6cd814
Alexia Ferric <alexia@ferric.fr>
2022-09-28 16:50:18 +0700

    Url api error

--- src/constants/config.tsx
+++ src/constants/config.tsx
@@ -1,4 +1,4 @@
-export const serverUrl = "https://sharecount-api.herokuapp.com:3000";
+export const serverUrl = "https://sharecount-api.herokuapp.com";
 
 // export const serverUrl = "http://localhost:3000";
 
