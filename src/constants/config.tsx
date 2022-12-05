158ed419e0d815aa68f5d341a1409215804e6aac
Alexia Ferric <alexia@ferric.fr>
2022-12-05 16:12:46 +0700

    update config

--- src/constants/config.tsx
+++ src/constants/config.tsx
@@ -1,4 +1,8 @@
+// LOCAL
+// export const clientUrl = "http://localhost:3006";
+// export const serverUrl = "http://localhost:3000";
+
+export const clientUrl = "https://sharecount-client.herokuapp.com";
 export const serverUrl = "https://sharecount-api.herokuapp.com";
 
-// export const serverUrl = "http://localhost:3000";
 
