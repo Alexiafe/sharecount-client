4deb72cd42cdb39800021596c20ba7dfcf690415
Alexia Ferric <alexia@ferric.fr>
2022-12-09 12:50:21 +0700

    Big style update

--- src/App.tsx
+++ src/App.tsx
@@ -17,42 +17,70 @@ import ExpenseEdit from "./screen/ExpenseEdit";
 // React
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+// MUI
+import { createTheme, ThemeProvider } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#5A7684",
+    },
+    secondary: {
+      main: "#DC5656",
+    },
+    text: {
+      primary: "#4F4F4F",
+      secondary: "#707070",
+    },
+  },
+  shape: {
+    borderRadius: 30,
+  },
+  typography: {
+    allVariants: {
+      color: "#4F4F4F",
+    },
+  },
+});
+
 const App = () => {
   return (
-    <AuthContextProvider>
-      <div>
-        <Router>
-          <Routes>
-            <Route path="/login" element={<Login />} />
+    <ThemeProvider theme={theme}>
+      <AuthContextProvider>
+        <div>
+          <Router>
+            <Routes>
+              <Route path="/login" element={<Login />} />
 
-            <Route path="/" element={<SharecountsList />} />
-            <Route
-              path="/sharecount-edit/:sharecountID"
-              element={<SharecountEdit />}
-            />
-            <Route path="/sharecount-add" element={<SharecountAdd />} />
-            <Route
-              path="/sharecount-connect/:sharecountID"
-              element={<SharecountConnect />}
-            />
+              <Route path="/" element={<SharecountsList />} />
+              <Route
+                path="/sharecount-edit/:sharecountID"
+                element={<SharecountEdit />}
+              />
+              <Route path="/sharecount-add" element={<SharecountAdd />} />
+              <Route
+                path="/sharecount-connect/:sharecountID"
+                element={<SharecountConnect />}
+              />
 
-            <Route path="/sharecount/:sharecountID" element={<Expenses />} />
-            <Route
-              path="/sharecount/:sharecountID/expense/:expenseID"
-              element={<ExpensesDetails />}
-            />
-            <Route
-              path="/sharecount/:sharecountID/expense-add"
-              element={<ExpenseAdd />}
-            />
-            <Route
-              path="/sharecount/:sharecountID/expense-edit/:expenseID"
-              element={<ExpenseEdit />}
-            />
-          </Routes>
-        </Router>
-      </div>
-    </AuthContextProvider>
+              <Route path="/sharecount/:sharecountID" element={<Expenses />} />
+              <Route
+                path="/sharecount/:sharecountID/expense/:expenseID"
+                element={<ExpensesDetails />}
+              />
+              <Route
+                path="/sharecount/:sharecountID/expense-add"
+                element={<ExpenseAdd />}
+              />
+              <Route
+                path="/sharecount/:sharecountID/expense-edit/:expenseID"
+                element={<ExpenseEdit />}
+              />
+            </Routes>
+          </Router>
+        </div>
+      </AuthContextProvider>
+    </ThemeProvider>
   );
 };
 
